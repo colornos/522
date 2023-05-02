d6f692db341f5187f75e150c562f640d3a7526b9
colornos <rivas432@hotmail.com>
2023-05-01 23:43:56 -0500

    Create save.py

---
+++ save.py
@@ -0,0 +1,22 @@
+from time import sleep
+import sys
+import RPi.GPIO as GPIO
+from mfrc522 import SimpleMFRC522
+
+reader = SimpleMFRC522()
+
+def save_to_file(id, text):
+    with open("rfid.txt", "a") as file:
+        file.write(f"ID: {id}\nText: {text}\n")
+
+try:
+    while True:
+        print("Hold a tag near the reader")
+        id, text = reader.read()
+        print("ID: %s\nText: %s" % (id, text))
+        save_to_file(id, text)
+        sleep(5)
+except KeyboardInterrupt:
+    print("Program terminated")
+    GPIO.cleanup()
+    sys.exit()
